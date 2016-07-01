6ce794a7b5a2001daf535a7639552c59bf0308bd
zzzzz-z <2277103364@qq.com>
2016-07-01 12:15:05 +0800

    第一次正式提交

---
+++ src/entity/Commodity.java
@@ -0,0 +1,24 @@
+package entity;
+
+/**
+ * Created by hjc on 16-7-1.
+ */
+public class Commodity {
+    //商品对象
+
+    private int c_id;
+    //商品id
+
+    private String c_name;
+    //商品名
+
+    private int commodity_class_cc_id;
+    //该商品的类别id
+
+    private int c_number;
+    //该商品的剩余数量
+    
+    private
+    private
+    private
+}
