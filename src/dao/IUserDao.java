281e92ee546f3e2d83962f336b5a63200f1f1b16
cxspace <442961832@qq.com>
2016-07-02 14:31:34 +0800

    用户dao接口

--- src/dao/IUserDao.java
+++ src/dao/IUserDao.java
@@ -1,8 +1,35 @@
 package dao;
 
+import entity.User;
+
+import java.sql.SQLException;
+
 /**
  * Created by cxspace on 16-7-1.
  */
 public interface IUserDao {
+    /*
+    *     注册逻辑
+    * */
+
+    /*
+          添加用户数据
+     */
+
+    public boolean add(User user);
+
+
+    /*
+          登录逻辑
+
+          功能：根据手机号,密码查询用户
+
+          返回一个用户对象
+     */
+
+    public User find(String phone , String password) throws SQLException;
+
+
+
 
 }
